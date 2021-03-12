abd736b8621294f46f3f0e3344a2542cd1248f73
Nicholas Agnihotri Burka <nicholas@Nicholass-MBP.home>
2021-03-12 16:56:41 -0500

    .

--- dist/views/PlayGame.vue
+++ dist/views/PlayGame.vue
@@ -36,7 +36,29 @@
 			<h3 v-if="currentgame.title" class="column">{{ currentgame.title }}</h3>
 			<h3 class="column" id="wager">{{currentgame.wagerreadable}} {{currentgame.currency}}</h3>
 		</div>
-		<PlayHands v-on:submithands="submithands" :rockCount="rockCount" :paperCount="paperCount" :scissorsCount="scissorsCount"></PlayHands>
+		<div class="column">
+			<div id="game" class="row" style="position: relative; height: 60vh;">
+				<div id="rock-cont" class="column">
+					<img id="rock" class="rps-piece" src="img/rock.png" alt="rock" v-on:click="addHand('Rock')">
+					<p class="count-used" style="position: relative; text-align: center; display: block;">{{rockCount}}</p>
+				</div>
+				<div id="paper-cont" class="column">
+					<img id="paper" class="rps-piece" src="img/paper.jpg" alt="paper" v-on:click="addHand('Paper')">
+					<p class="count-used" style="position: relative; text-align: center; display: block;">{{paperCount}}</p>
+				</div>
+				<div id="scissors-cont" class="column">
+					<img id="scissors" class="rps-piece" src="img/scissors2.png" alt="scissors" v-on:click="addHand('Scissors')">
+					<p class="count-used" style="position: relative; text-align: center; display: block;">{{scissorsCount}}</p>
+				</div>
+			</div>
+			<div id="chosen" class="row" style="height: 20vh; background-color: purple">
+				<img v-for="hand in hands" v-bind:id="hand" class="rps-piece" v-bind:src="'img/' + hand + '.png'" v-bind:alt="hand" v-on:click="rmoveHand(hand)">
+			</div>
+			<div id="submit" class="row">
+				<button v-on:click="submithands()"></button>
+			</div>
+		</div>
+		<!--<PlayHands v-on:submithands="submithands" :rockCount="rockCount" :paperCount="paperCount" :scissorsCount="scissorsCount"></PlayHands>-->
 		<!--<div id="history" class="">
 			<div class="row">
 				<img src="" draggable="true" ondragstart="dragstarted(event)" ondragover="draggingover(event)"  ondrop="dropped(event)" alt="rock" class="column">
@@ -62,7 +84,8 @@
 		props: ["currentgame", "rockCount", "paperCount", "scissorsCount"],
 		data: function() {
 			return {
-				chosen_hands: []
+				chosen_hands: [],
+				hands: []
 			}
 		}, 
 		methods: {
@@ -75,6 +98,21 @@
 			submithands: function(hands) {
 				//this.currentgame.resolveHands(hands);
 				this.$emit('submithands', this.currentgame, hands);
+			},
+			addHand: function(hand) {
+				this.hands.push(hand);
+			},
+			/*removeHand: function() {
+				this.hands.pop();
+			},*/
+			removeHand: function(hand) {
+				const index = array.indexOf(hand);
+				if (index > -1) {
+				  this.hands.splice(index, 1);
+				}
+			},
+			submithands: function() {
+				this.$emit('submithands', this.hands);
 			}
 		}
 	}
