dcc15f13f29873d238d3ec17f285bcf7765abc8c
Nicholas Agnihotri Burka <nicholas@Nicholass-MBP.home>
2021-03-12 16:23:10 -0500

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -231,7 +231,7 @@ ul {
 
 				console.log(gamesold);
 				console.log(gamesnew);
-				console.log(opengames);
+				console.log(this.opengames);
 				this.playableGames = this.gamesnew.filter((x) => {return x.playable});
 				console.log(this.playableGames);
 				console.log(this.playable_games);
