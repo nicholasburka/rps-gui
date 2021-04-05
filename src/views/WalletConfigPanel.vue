cbb75f4c58ab21c29081349c072dc56bb6aa0aa6
nicholasburka <nburka@mac.com>
2021-04-05 15:31:59 -0400

    quickdeploy: walletconfig css tweaks

--- src/views/WalletConfigPanel.vue
+++ src/views/WalletConfigPanel.vue
@@ -2,7 +2,7 @@
 #wallet_config {
 	z-index: 10;
 	position: absolute;
-	height: 60vh;
+	padding-bottom: 5vh;
 	top: 20vh;
 	width: 70vw;
 	left: 15vw;
@@ -11,7 +11,7 @@
 }
 #x {
 	top: 5%;
-	left: 5%;
+	left: 1vw;
 	max-height: 7vh;
 	max-width: auto;
 	background-color: black;
@@ -21,8 +21,7 @@
 	z-index: 5;
 	height: 7vh;
 	width: auto;
-	top: 2vh;
-	left: 2vh;
+	top: -5vh;
 }
 .smaller {
 	font-size: 80%;
@@ -30,6 +29,11 @@
 a {
 	margin-left: 4px;
 	margin-right: 4px;
+}
+button {
+	display: flex;
+	position: relative;
+	flex-basis: 40%;
 }
 </style>
 
@@ -39,29 +43,30 @@ a {
 		<div id="refresh">
 			<button id="connectWallets" v-on:click="connectWallets()">Refresh</button>
 		</div>-->
-		<div>
-			<img id="x" src="../img/x2.png" v-on:click="dismissWalletConfig()">
-			<img id="faucet" src="../img/faucet.png" v-on:click="tryfaucet()">
-			<h3 class="row smaller"> Connect a cryptocurrency wallet using <a href="https://metamask.io/download.html">&nbsp;&nbsp;MetaMask</a>&nbsp;or [soon available, only on Betanet]<a href="https://www.purestake.com/technology/algosigner/">&nbsp;AlgoSigner</a></h3>
-
-			<div class="row"> 
-				<div id="ETH">
-					<!--<h3>current ETH wallet: {{current_eth}}</h3>-->
-					<button class="connectWallet" v-on:click="connectWallet('ETH')">(re)connect ETH</button>
-				</div>
-				<div id="ALGO">
-					<!--<h3>current ALGO wallet: {{current_algo}}</h3>-->
-					<button class="connectWallet" v-on:click="connectWallet('ALGO')">(re)connect ALGO</button>
-				</div>
+			<div class="row">
+				<img id="x" src="../img/x2.png" v-on:click="dismissWalletConfig()">
+				<img id="faucet" src="../img/faucet.png" alt="faucet (only available on local devnets)"v-on:click="tryfaucet()">
 			</div>
+			<h3 class="row smaller"> Connect a cryptocurrency wallet using ETH <a href="https://metamask.io/download.html">&nbsp;&nbsp;MetaMask</a>&nbsp;or [available soon on Algorand Betanet, Chrome only]<a href="https://www.purestake.com/technology/algosigner/">&nbsp;AlgoSigner</a></h3>
 			<!--<div v-if="this.walletAddress" id="current" class="flex-center">
 				<h3 class="smalltext">{{ this.walletAddress }}</h3>
 				<div class="row"><h3> {{ this.thebalance }} </h3><h3 class="currency"> {{ this.currency }} </h3><img class="currency-icon" v-bind:src="imsrc(this.currency)"></div>
 			</div>-->
-			<img v-if="this.walletLoading" src="../img/loading.gif"/>
-			<h3 v-if="this.walletErr">{{ this.walletErr }}</h3>
+			<div id="status" style="height: 20vh; object-fit: contain">
+				<img v-if="this.walletLoading" src="../img/loading.gif"/>
+				<h3 v-if="this.walletErr">{{ this.walletErr }}</h3>
+			</div>
+			<div id="buttons" class="row" style="display:flex; justify-content: space-around; position: relative;"> 
+				<div id="ETH" class="button">
+					<!--<h3>current ETH wallet: {{current_eth}}</h3>-->
+					<button class="connectWallet" v-on:click="connectWallet('ETH')">(re)connect ETH</button>
+				</div>
+				<div id="ALGO" class="button">
+					<!--<h3>current ALGO wallet: {{current_algo}}</h3>-->
+					<button class="connectWallet" v-on:click="connectWallet('ALGO')">(re)connect ALGO</button>
+				</div>
+			</div>
 			<AmountCurrencyDisplay v-if="this.walletAddress && !this.walletLoading" v-bind:amount="this.balance" v-bind:currency="this.currency"></AmountCurrencyDisplay>
-		</div>
 	</div>
 </template>
 
