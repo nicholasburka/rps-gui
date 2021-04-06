b62d569933a4987e0688b314d00495af0ca31fca
nicholasburka <nburka@mac.com>
2021-04-06 17:10:56 -0400

    quickdeploy: search

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -41,6 +41,7 @@ li:hover {
   }
 #play {
 	height: 100vh;
+	width: 100vw;
 }
 #accept {
 	height: 100vh;
@@ -48,14 +49,45 @@ li:hover {
 </style>
 
 <template>
-	<div id="game-search-results" class="column" style="margin-top:2vh;">
+	<div id="game-search-results" class="column" style="">
 		<div v-if="!this.play">
 			<img id="back-arrow" src="../img/back-arrow.png" alt="back" v-on:click="$router.go(-1);">
 			<!--<SearchResult v-for="game in searchResults" v-bind:game="game" v-bind:title="game.title" v-bind:wager="game.wagerreadable" v-bind:playerAddr="game.p1" v-bind:style="{'background-color': randomcolor()}" v-on:click.native="onclick(game);">
 			</SearchResult>!-->
 			<li v-for="game in searchResults" v-on:click="onclick(game)" v-bind:style="{'background-color': randomcolor()}">{{game.wagerreadable}} {{game.currency}} - {{game.title}} - {{game.status}} - made by {{ game.p1 }} </li>
 		</div>
-		<PlayGame id="play" v-if="this.play" :game="this.game" v-on:back="cancelPlay()" v-on:submithands="this.submitHands"></PlayGame>
+		<!--<PlayGame id="play" v-if="this.play" :game="this.game" v-on:back="cancelPlay()" v-on:submithands="this.submitHands"></PlayGame>-->
+		<div v-if="this.play">
+			<img id="back-arrow" src="../img/back-arrow.png" alt="back" v-on:click="this.back">
+			<div class="row" id="game-header">
+				<h3 v-if="game.title" class="column">{{ game.title }}</h3>
+				<h3 class="column" id="wager">{{game.wagerreadable}} {{game.currency}}</h3>
+			</div>
+			<h3 class="row">Winner chosen by first non-tie hand</h3>
+			<div class="row" style="height: 80vh;">
+				<div id="game" class="row" style="position: relative; height: 40vh;">
+					<div id="rock-cont" class="column">
+						<img id="rock" class="rps-piece" src="../img/rock.png" alt="rock" v-on:click="addHand(0)">
+						<p class="count-used" style="position: relative; text-align: center; display: block;">{{rockCount}}</p>
+					</div>
+					<div id="paper-cont" class="column">
+						<img id="paper" class="rps-piece" src="../img/paper.jpg" alt="paper" v-on:click="addHand(1)">
+						<p class="count-used" style="position: relative; text-align: center; display: block;">{{paperCount}}</p>
+					</div>
+					<div id="scissors-cont" class="column">
+						<img id="scissors" class="rps-piece" src="../img/scissors2.png" alt="scissors" v-on:click="addHand(2)">
+						<p class="count-used" style="position: relative; text-align: center; display: block;">{{scissorsCount}}</p>
+					</div>
+				</div>
+				<div id="chosen" class="row" style="height: 25vh; background-color: purple" v-on:click="removeLastHand()">
+					<img v-for="hand in hands" v-bind:id="hand" class="rps-piece" v-bind:src="imsrc(hand)" v-bind:alt="hand" v-on:click="removeHand(hand)">
+				</div>
+				<div id="submit" class="row" style="height: 10vh">
+					<!--<h3 v-if="err">{{err}}</h3>-->
+					<button v-on:click="submithands()">Shoo!</button>
+				</div>
+			</div>
+		</div>
 		<AcceptGame id="confirm" v-if="this.confirm" :game="this.game" :blocktime="100" :currency="currency" v-on:confirm="confirmgame()" v-on:deny="deny()"></AcceptGame>
 	</div>
 </template>
