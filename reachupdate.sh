e156501227dc5d63b5f6ce2b6de1dd315db7e96c
nicholasburka <nburka@mac.com>
2021-04-26 19:12:06 -0400

    new reach version

---
+++ reachupdate.sh
@@ -0,0 +1,10 @@
+docker container kill $(docker ps -q);
+./reach/reach clean;
+./reach/reach update;
+
+npm update @reach-sh;
+
+./reach/reach compile ./reach/index.rsh;
+mv ./reach/build/index.main.mjs ./build/index.main.mjs;
+
+npm run build src/index.js;
